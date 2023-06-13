5da9aa7146dafa0a61b211e3efc192274078ee02
Shaun D <shaun@deepfriedlabs.com>
2023-06-13 18:49:21 +0100

    remove dist

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.appendEllipsis = void 0;
-function appendEllipsis(inputString) {
-    return "".concat(inputString, "...");
-}
-exports.appendEllipsis = appendEllipsis;
